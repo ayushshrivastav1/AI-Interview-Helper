a4810150296e1e8de55bf7f6c0b4bd9092ae8c87
ayushshrivastav1 <ayushshrivastav2151@gmail.com>
2026-06-22 07:03:50 +0530

    Update and test user authentication logic

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require ("express")
+const authRouter = require('./routes/auth.routes.js'); 
 
 const app = express()
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
-
+const bcrypt =  require("bcryptjs")
+const jwt = require ("jsonwebtoken")
 
 /**
  * @name registerUserController
@@ -17,17 +18,73 @@ async function registerUserController(req,res) {
             message : "please provide username,email, password"
         })
     }
-    const isUserALreadyExists = await userModel.findOne({
+    const isUserAlreadyExists = await userModel.findOne({
         $or: [ { username } , { email } ]
     })
 
-    if(isUserAlredyExists) {
+    if(isUserAlreadyExists) {
         return res.status (400).json({
             message: "Account already exist with this email address or username"
         })
     }
-} 
+
+
+const hash = await bcrypt.hash(password, 10)
+
+const user = await userModel.create({
+    username,
+    email,
+    password: hash
+})
+
+
+const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+
+res.cookie("token", token)
+
+
+res.status(201).json({
+    message: "User Registered Successfully",
+    user: {
+        id: user._id,
+        username: user.username,
+        email: user.email
+    }
+})
+
+}
+
+/**
+ * @name loginUserController
+ */
+async function loginUserController(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        
+        const user = await userModel.findOne({ email });
+
+        
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+    
+        return res.status(200).json({ 
+            message: "Login placeholder success", 
+            user 
+        });
+
+    } catch (err) {
+        // 4. Catch block prevents the server from hanging if a database error happens
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+
 
 module.exports = { 
-    registerUserController
+    registerUserController,
+    loginUserController
 }
